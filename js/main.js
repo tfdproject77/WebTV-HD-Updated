7e923c61564b1df32b166e33c09dcf6f117a7781
coltonsr77 <coltonr3@icloud.com>
2026-04-10 18:38:54 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 'use strict';
 //Redirect if the user isn't using the iframe page
-if(self===top){location.replace('https://SKCro.github.io/WebTV-HD/?page='+location.pathname);}
+if(self===top){location.replace('https://webtvsim.coltonsr77.net/?page='+location.pathname);}
 
 //Sound handling, tabindex, and hover effect
 function playSound(snd){parent.postMessage({type:'sound',soundType:snd},'*');}
 addEventListener('DOMContentLoaded',function(){
 	//Import selection box code
 	const selBoxScript=document.createElement('script');
-	selBoxScript.src='https://SKCro.github.io/WebTV-HD/js/selectionBox.js';
+	selBoxScript.src='https://webtvsim.coltonsr77.net/js/selectionBox.js';
 	document.head.appendChild(selBoxScript);
 
 	const inputs=document.querySelectorAll('.input');
